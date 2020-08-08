9728b473731e328fb8c4393d4f8b91ce37faa1c3
zhuchifeng <18515592159@163.com>
2020-08-08 12:02:58 +0800

    first init

---
+++ nacos-discovery/src/main/java/com/itheima/nacos/provider/controller/RestProviderController.java
@@ -0,0 +1,15 @@
+package com.itheima.nacos.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestProviderController {
+
+    //暴露RESTful接口
+    @GetMapping(value = "/service")
+    public String service() {
+        System.out.println("provider invoke");
+        return "provider invoke";
+    }
+}
